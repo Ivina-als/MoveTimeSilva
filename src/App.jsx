4efd5d93627de020f36f39c45caf358af316b2c5
ivina albuquerque <ivina.albuquerque.s@gmail.com>
2024-03-05 19:20:04 -0300

    creaded NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import light from "/light.png";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
+      <NavBar />
       <div className="container-title">
         <div className="container-figure">
           <img className="container-figure-img" src={light} />
